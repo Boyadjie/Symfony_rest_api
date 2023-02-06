594512e709b016d9fe4ffe6b5ad79b3eee5eb95d
pch-emeric <emeric.boyadjian@pch.click>
2023-02-06 11:29:35 +0100

    Completed and tested route/controller to create a book without not existing author

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
 use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -20,10 +25,35 @@ class BookController extends AbstractController
     }
 
     #[Route('/api/book/{id}', name: 'app_single_book', methods: ['GET'])]
-    public function getSingleBook(BookRepository $bookRepository, $id, SerializerInterface $serializer) {
+    public function getSingleBook(BookRepository $bookRepository, $id, SerializerInterface $serializer): JsonResponse
+    {
         $book = $bookRepository->findOneBy(['id'=>$id]);
         $jsonBook = $serializer->serialize($book, 'json', ['groups' => 'getBooks']);
 
         return new JsonResponse($jsonBook, 200, [], true);
+    }
+
+    #[Route('/api/book', name: 'new_book', methods: ['POST'])]
+    public function createSingleBook(BookRepository $bookRepository, Request $request, SerializerInterface $serializer, EntityManagerInterface $em, UrlGeneratorInterface $generator): JsonResponse
+    {
+        $serializedBody = $request->getContent();
+        $jsonBody = json_decode($serializedBody, true); // => null
+
+        try {
+            $isExistingBook = $bookRepository->findOneBy(['title' => $jsonBody['title']]);
+            if ($isExistingBook) {
+                return new JsonResponse($isExistingBook->getTitle() . " Already exist !", 200);
+            }
+        } catch (RouteNotFoundException $e) {
+            echo $e;
+        }
+
+        $body = $serializer->deserialize($serializedBody, Book::class, "json");
+        $em->persist($body);
+        $em->flush();
+        $jsonBook = $serializer->serialize($body, 'json', ['groups' => 'getBooks']);
+
+        $location = $generator->generate("new_book", ['id' => $body->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+        return new JsonResponse($jsonBook, 201, ["location" => $location], true);
     }
 }
--- tests/BookTest.php
+++ tests/BookTest.php
@@ -3,20 +3,34 @@
 namespace App\Tests;
 
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+use App\Entity\Author;
+use App\Entity\Book;
+use Faker;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class BookTest extends ApiTestCase
 {
-    private $client;
+    protected static $client;
+    protected static $serializer;
+
     protected function setUp(): void
     {
         // "client" that is acting as the browser
-        $this->client = static::createClient();
+        self::$client = static::createClient();
+        $this->faker = Faker\Factory::create('fr_FR');
+
+        $encoders = [new XmlEncoder(), new JsonEncoder()];
+        $normalizers = [new ObjectNormalizer()];
+        self::$serializer = new Serializer($normalizers, $encoders);
     }
 
     public function testGetAllBooks(): void
     {
         // Request a specific page
-        $crawler = $this->client->request('GET', '/api/book');
+        $crawler = self::$client->request('GET', '/api/book');
         // Validate a successful response
         $this->assertResponseIsSuccessful();
         $this->assertCount(20, $crawler->toArray());
@@ -24,7 +38,17 @@ class BookTest extends ApiTestCase
 
     public function testGetSingleBook(): void
     {
-        $response = $this->client->request('GET', '/api/book/2');
+        $response = self::$client->request('GET', '/api/book/2');
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testCreateBook(): void
+    {
+        $body = '{
+            "title": "Super title",
+            "coverText": "Nulla numquam dolor numquam quo. Quas nam nobis consequuntur soluta impedit."
+        }';
+        $response = self::$client->request('POST','/api/book',['body' => $body]);
         $this->assertResponseIsSuccessful();
     }
 }
